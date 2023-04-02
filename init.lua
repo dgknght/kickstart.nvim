f1bca069b0cac2510ab9b26bd3dd19219819b5af
Doug Knight <dougbknight@gmail.com>
2023-04-02 10:43:58 -0500

    change onedark style to deep

--- init.lua
+++ init.lua
@@ -42,6 +42,10 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
+vim.g.onedark_config = {
+  style = 'deep'
+}
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
