7cdb70e3c2d1940e6db17c1652a556f10d08cbea
Doug Knight <dougbknight@gmail.com>
2024-03-29 11:03:45 -0500

    add codium plugin

--- init.lua
+++ init.lua
@@ -272,6 +272,11 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter'
+  }
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
