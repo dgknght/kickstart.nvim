81f5261dbee427ba5d02d0f9c31e45787bf6246d
Doug Knight <dougbknight@gmail.com>
2026-03-06 15:57:21 -0600

    disable miniai for clojure files

--- init.lua
+++ init.lua
@@ -238,6 +238,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+-- Disable mini's pair handling for clojure files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'clojure' },
+  callback = function() vim.b.miniai_disable = true end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
@@ -917,7 +923,7 @@ require('lazy').setup({
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
       local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-      require('nvim-treesitter.configs').setup({ ensure_installed = parsers })
+      require('nvim-treesitter.configs').setup { ensure_installed = parsers }
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
           local buf, filetype = args.buf, args.match
