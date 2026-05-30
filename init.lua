4e0ae1ffef1a39cfc460d063d2d1edb332086dc4
Doug Knight <dougbknight@gmail.com>
2026-05-30 11:58:29 -0500

    disable unneeded language providers

--- init.lua
+++ init.lua
@@ -108,6 +108,11 @@ do
 
   vim.g.autoformat = false
 
+  -- Disable unused language providers to suppress healthcheck noise
+  vim.g.loaded_python3_provider = 0
+  vim.g.loaded_ruby_provider = 0
+  vim.g.loaded_perl_provider = 0
+
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = true
 
